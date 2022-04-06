9b86b01eb955bec6a4177394f05277f74ed82ff7
Wojciech Czepukojć <wojciechczepukojc@gmail.com>
2022-04-06 21:41:10 +0200

    sosna
    olcha

---
+++ src/main/java/pl/sda/arppl/pracawzespole/Drzewa.java
@@ -0,0 +1,13 @@
+package pl.sda.arppl.pracawzespole;
+
+public class Drzewa {
+    String l_i;
+    int wiek;
+    String kolor;
+
+    public Drzewa(String l_i, int wiek, String kolor) {
+        this.l_i = l_i;
+        this.wiek = wiek;
+        this.kolor = kolor;
+    }
+}
--- src/main/java/pl/sda/arppl/pracawzespole/Main.java
+++ src/main/java/pl/sda/arppl/pracawzespole/Main.java
@@ -2,7 +2,13 @@ package pl.sda.arppl.pracawzespole;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Próba mikrofonu");
+
+        Drzewa sosna = new Drzewa("igła", 20, "beż");
+        System.out.println(sosna);
+
+        Drzewa olcha = new Drzewa("liść", 30, "szary");
+
+
 
     }
 
