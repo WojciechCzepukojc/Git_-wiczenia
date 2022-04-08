a5fb450891aa35305bb68651b15abc40268de47c
Wojciech Czepukojć <wojciechczepukojc@gmail.com>
2022-04-08 20:56:57 +0200

    Zmiana zatwierdzona

--- src/main/java/pl/sda/arppl/pracawzespole/Main.java
+++ src/main/java/pl/sda/arppl/pracawzespole/Main.java
@@ -3,6 +3,30 @@ package pl.sda.arppl.pracawzespole;
 public class Main {
     public static void main(String[] args) {
 
-    }
+        int result = (int) (Math.random() * 6 + 1);
+       switch (result) {
+           case 1 :
+           case 3 :
+           case 5 :
+               System.out.println("Wyrzucono nieparzystą liczbę oczek");
+               break;
+           case 2 :
+           case 4 :
 
+               System.out.println("Wyrzucono parzystą liczbę oczek");
+               break;
+           case 6 :
+               System.out.println("Wyrzucono parzystą liczbę oczek");
+               System.out.println("WYGRALES");
+               break;
+           default:
+               System.out.println("Jesteś najebany, rzuć jeszcze raz" );
+
+
+
+       }
+
+
+
+    }
 }
